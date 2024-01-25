44e2bc8fb24f5582b98f045f9a88173aacf4030e
Dmitry Yakorev <urakend@mail.ru>
2024-01-25 22:46:49 +0300

    feat:new2_up1

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -37,8 +37,17 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  return Array.from(
+    Array(Math.max(arr1.length, arr2.length)).fill(0),
+    (_, i) => {
+      let res = 0;
+      if (arr1[i]) res = arr1[i];
+      if (arr2[i]) res = arr2[i];
+      if (arr1[i] && arr2[i]) res = arr1[i] + arr2[i];
+      return res;
+    }
+  );
 }
 
 /**
