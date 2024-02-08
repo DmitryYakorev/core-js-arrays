883658432876c5c4fc9f374175e0b20a76b75d61
Dmitry Yakorev <urakend@mail.ru>
2024-02-08 14:31:48 +0300

    feat:new13_up2

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -349,8 +349,11 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  const array = new Array(Math.ceil(arr.length / chunkSize)).fill([]);
+  return array.map((el, i) =>
+    el.concat(arr.slice(i * chunkSize, (i + 1) * chunkSize))
+  );
 }
 
 /**
